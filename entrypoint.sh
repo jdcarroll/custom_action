581064f0375e43dbc05f35e05071125a920d0141
Jeffrey Carroll <jcarroll@jeffreys-mbp-2.lan>
2023-10-01 14:50:23 -0400

    testing iteration with loop in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+my_string=$1
+IFS=';' read -ra my_array <<< "$my_string"
+
+#Print the split string
+for i in "${my_array[@]}"
+do
+    echo "Hello $i"
+done
+
+echo 
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
